73e32cead4c7f73cc39abd54e99283a04d16196b
drewszczepanski <67236595+drewszczepanski@users.noreply.github.com>
2020-06-21 15:50:51 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -6,7 +6,7 @@ Team 4
 Developed: June 14, 2020
 Last date Changed: June 21, 2020
 Rev: 1
-/*
+*/
 //Have to create an arrayList to add customer information
 //Have to add print Cust method
 package edu.psu.abington.ist.ist242;
