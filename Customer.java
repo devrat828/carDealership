e5ddeb220030a896a63842c91fea530ef098b5af
drewszczepanski <67236595+drewszczepanski@users.noreply.github.com>
2020-06-21 15:49:06 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,7 +1,14 @@
-//*
+/*
+Project: Car Design
+Purpose Details: Car Dealership Application
+Course: IST 242
+Team 4
+Developed: June 14, 2020
+Last date Changed: June 21, 2020
+Rev: 1
+/*
 //Have to create an arrayList to add customer information
 //Have to add print Cust method
-//*
 package edu.psu.abington.ist.ist242;
 import java.util.*;
 public class Customer {
