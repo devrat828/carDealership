a5dc0e007a5955475d2f54820f692c90cf3e1185
gsywulak <67237847+gsywulak@users.noreply.github.com>
2020-06-21 15:46:13 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 package edu.psu.abington.ist.ist242;
 
 public class Main {
-
+//Starting point of the program
     public static void main(String[] args) {
-         Dealership dealership = new Dealership();
+         Dealership dealership = new Dealership();//creates the dealership class and calls it to run
          dealership.runDealership();
 
     }
